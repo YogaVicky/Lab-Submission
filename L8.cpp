1f7e47a9569b823b00ca69bef91ea3c9549239a9
imjohnzakkam <johnzakkam2592@gmail.com>
2020-03-28 21:00:06 +0530

    u

--- L8.cpp
+++ L8.cpp
@@ -1,3 +1,15 @@
 #include<bits/stdc++.h>
 #include"makefiles8/Test_bipartite.h"
+//#define Graph vector<vector<ll>>
+#define ll long long int
+using namespace std;
 
+int main()
+{
+    ll v;
+    cout << "enter the number of vertices : ";
+    cin >> v;
+    ll Graph[v][v];
+    
+    return 0;
+}
