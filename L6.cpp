5531477bb3ffbf46e04fd0fa69f90a297e2461bf
Paleti Krishnasai <paletikrishnasai245@gmail.com>
2020-03-28 17:40:34 +0530

    Update L6.cpp

--- L6.cpp
+++ L6.cpp
@@ -1,7 +1,10 @@
 #include<bits/stdc++.h>
-#include"makefiles5/nrootn.h"
+#include"nrootn.h"
+#include"2n-3.h"
 #define ll long long int
+
 using namespace std ;
+
 
 int main()
 {
@@ -11,4 +14,18 @@ int main()
     ll z=primalitycheck(n);
     if(z==0) cout<<"not prime"<<endl;
     if(z==1) cout<<"prime"<<endl;
+
+    int number;
+	cout<<"enter the size"<<endl;
+	cin>>number;
+	int arr[number];
+	cout<<"enter"<<number<<"elements"<<endl;
+	for (int i = 0; i < number; ++i)
+	{
+		cin>>arr[i];
+	}
+	struct test t=comp(arr,number);
+	cout<<"max="<<t.max<<endl;
+	cout<<"min="<<t.min<<endl;
+
 }
