b450bdbae47eb6a21056ea852b16f2a8eaea2d04
Paleti Krishnasai <paletikrishnasai245@gmail.com>
2020-03-28 13:10:41 +0530

    Update L5.cpp

--- L5.cpp
+++ L5.cpp
@@ -1,13 +1,14 @@
 #include<bits/stdc++.h>
-#include<nrootn.h>
+#include"nrootn.h"
 #define ll long long int
 using namespace std ;
 
 int main()
 {
+	ll n;
 	cout<<"Enter n :"<<endl;
     cin>>n;
-    ll z=nrootn(n);
-    if(z==0) cout<<"not prime"<<endl
-    if(z==1) cout<<"prime"<<endl
+    ll z=primalitycheck(n);
+    if(z==0) cout<<"not prime"<<endl;
+    if(z==1) cout<<"prime"<<endl;
 }
