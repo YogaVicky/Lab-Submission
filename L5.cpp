f55374bc394d605db99617b3ea28c60d0dfd1c06
Paleti Krishnasai <paletikrishnasai245@gmail.com>
2020-03-28 13:05:21 +0530

    Add files via upload

---
+++ L5.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<nrootn.h>
+#define ll long long int
+using namespace std ;
+
+int main()
+{
+	cout<<"Enter n :"<<endl;
+    cin>>n;
+    ll z=nrootn(n);
+    if(z==0) cout<<"not prime"<<endl
+    if(z==1) cout<<"prime"<<endl
+}
